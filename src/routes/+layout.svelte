a4cacbee8e6fd07bb5dfcf7c6020db743172b4ac
lov2fi <lov2fi@gmail.com>
2023-02-12 23:36:49 +0900

    layout, fetch

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<h1>
+  <a href="/">home</a>
+  <a href="/about">about</a>
+</h1>
+<slot />
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let data;
+  // console.log(data);
+  const { products } = data;
+  const { users } = data;
+</script>
+
+{#each products as product}
+  <h2>{product.title}</h2>
+  <h3>($ {product.price})</h3>
+{/each}
+
+{#each users as user}
+  <p>{user.firstName}({user.age})</p>
+{/each}
