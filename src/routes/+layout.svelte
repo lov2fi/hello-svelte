2371754a977a200baf5a048a7ba57ce448a83f5c
lov2fi <lov2fi@gmail.com>
2023-02-16 23:11:49 +0900

    posting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,23 @@
 <script>
   import '../app.css';
   import { base } from '$app/paths';
+
+  const toggleMode = () => {
+    if (document.documentElement.classList.contains('dark')) {
+      document.documentElement.classList.remove('dark');
+    } else {
+      document.documentElement.classList.add('dark');
+    }
+  };
 </script>
 
-<h1 class="fond-bold space-x-2 text-3xl underline">
-  <a href="{base}/">home</a>
-  <a href="{base}/about">about</a>
-</h1>
-<slot />
-
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.orange.100);
-  }
-</style>
+<div class="h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-4">
+  <div class="flex flex-row">
+    <h1 class="fond-bold flex-auto space-x-2 text-3xl underline">
+      <a href="{base}/">home</a>
+      <a href="{base}/about">about</a>
+    </h1>
+    <button class="rounded-lg bg-slate-800 p-2 text-sm text-white shadow-xl" on:click={toggleMode}>mode</button>
+  </div>
+  <slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,12 @@
 <script>
-  const toggleMode = () => {
-    if (document.documentElement.classList.contains('dark')) {
-      document.documentElement.classList.remove('dark');
-    } else {
-      document.documentElement.classList.add('dark');
-    }
-  };
+  import { base } from '$app/paths';
 </script>
 
-<div class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-4">
-  <div class="m-10 rounded-lg bg-white p-6 shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-800">
-    <h3 class="mt-2 text-base font-medium tracking-tight text-slate-900 dark:text-white">Sveltekit & Tailwindcss</h3>
-    <p class="mt-2 mb-2 text-sm text-slate-500 dark:text-slate-400">
-      Noto is a global font collection for writing in all modern and ancient languages. Noto Sans KR is an unmodulated
-      (“sans serif”) design for the Korean language using Hangul and the Korean Hanja scripts. It also supports
-      Hiragana, Katakana, Latin, Cyrillic and Greek. It has multiple weights.한글
-    </p>
-    <button class="rounded-lg bg-red-200 p-2 text-sm shadow-xl" on:click={toggleMode}>dark mode</button>
-  </div>
+<div class="m-10 rounded-lg bg-white p-6 shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-800">
+  <img class="object-cover" src="{base}/pic/pexels-pixabay-40803.jpg" alt="" />
+  <h3 class="mt-2 text-base font-medium tracking-tight text-slate-900 dark:text-white">속박을 기다리는 사람</h3>
+  <p class="mt-2 mb-2 text-sm text-slate-500 dark:text-slate-400">
+    구원의 손길을 내밀어줄 누군가를 기다리고 있는가. 이러한 자들이 진심으로 손꼽아 기다리는 것은 옴짝달싹 못하게 자신을
+    속박해줄 사람이다. 채찍을 든 맹수조련사처럼 먹이를 주면서 능숙하게 조련해줄 바로 그런 지배자이다.
+  </p>
 </div>
