a84228e3094c151c733bc8cb01795d5c9b1ee41c
lov2fi <lov2fi@gmail.com>
2023-02-19 16:57:22 +0900

    image to url

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,11 @@
 </script>
 
 <div class="m-10 rounded-lg bg-white p-6 shadow-xl ring-1 ring-slate-900/5 dark:bg-slate-800">
-  <img class="object-cover" src="{base}/pic/pexels-pixabay-40803.jpg" alt="" />
+  <img
+    class="object-cover"
+    src="https://images.pexels.com/photos/40803/lion-animal-predator-big-cat-40803.jpeg?auto=compress&cs=tinysrgb&fit=crop&h=500&w=1200"
+    alt=""
+  />
   <h3 class="mt-2 text-base font-medium tracking-tight text-slate-900 dark:text-white">속박을 기다리는 사람</h3>
   <p class="mt-2 mb-2 text-sm text-slate-500 dark:text-slate-400">
     구원의 손길을 내밀어줄 누군가를 기다리고 있는가. 이러한 자들이 진심으로 손꼽아 기다리는 것은 옴짝달싹 못하게 자신을
