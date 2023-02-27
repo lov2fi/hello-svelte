aa6508718a52c6fec78563c153eed685a6d0c8f8
lov2fi <lov2fi@gmail.com>
2023-02-27 21:44:18 +0900

    regex

---
+++ src/routes/regex/+page.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  let regex: string = '[a-z]{2}\\d\\D';
+  let txt: string = 'ab0c1d ef2g3hij';
+  let answer: string | undefined = '';
+
+  const whenTyping = () => {
+    try {
+      let regexp = new RegExp(regex);
+      answer = getAnswer(txt, regexp)?.join() ?? ' ';
+    } catch {
+      answer = '[Expression Error]';
+    }
+  };
+
+  const getAnswer = (str: string, regexp: RegExp) => str.match(new RegExp(regex));
+
+  whenTyping();
+</script>
+
+<div class="mt-10 border-2 border-red-400">
+  <h1>sample</h1>
+  <input class="w-full" bind:value={txt} on:keyup={whenTyping} />
+</div>
+<div class="mt-2 border-2 border-red-400">
+  <h1>regex</h1>
+  <input class="w-full" bind:value={regex} placeholder="regular expression" on:keyup={whenTyping} />
+</div>
+<div class="mt-2 border-2 border-red-400">
+  <h1>result</h1>
+  <input class="w-full font-bold italic" bind:value={answer} />
+</div>
