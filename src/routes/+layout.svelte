7acd1b57fa314d642cf64362e7e0cf1c2d522afb
lov2fi <lov2fi@gmail.com>
2023-03-01 22:34:43 +0900

    layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,15 +11,24 @@
   };
 </script>
 
-<div class="h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-4">
-  <div class="flex flex-row">
-    <h1 class="fond-bold flex-auto space-x-2 text-3xl underline">
-      <a href="{base}/">home</a>
-      <a href="{base}/modern">modern</a>
-      <a href="{base}/regex">regex</a>
-      <a href="{base}/about">about</a>
-    </h1>
-    <button class="rounded-lg bg-slate-800 p-2 text-sm text-white shadow-xl" on:click={toggleMode}>mode</button>
-  </div>
-  <slot />
-</div>
+<header>
+  <nav class="flex items-center justify-between bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-4">
+    <div class="p-2 font-bold text-purple-300">
+      <a href="{base}/">🥩 hello-svelte</a>
+    </div>
+    <ul class="flex justify-around">
+      <li class="rounded-lg p-2 hover:bg-amber-500"><a href="{base}/modern">modern</a></li>
+      <li class="rounded-lg p-2 hover:bg-amber-500"><a href="{base}/regex">regex</a></li>
+      <li class="rounded-lg p-2 hover:bg-amber-500"><a href="{base}/about">about</a></li>
+    </ul>
+    <div>
+      <button class="rounded-lg bg-slate-800 p-2 text-sm text-white hover:bg-slate-500" on:click={toggleMode}
+        >mode</button
+      >
+    </div>
+  </nav>
+</header>
+<main class="h-screen bg-gray-100 pt-4 dark:bg-gray-400"><slot /></main>
+<footer class="bg-gray-100 dark:bg-gray-400">
+  <p class="text-center">testing...</p>
+</footer>
